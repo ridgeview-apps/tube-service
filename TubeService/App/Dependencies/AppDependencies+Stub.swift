ea7f1b060953ac6f5c4ba61ed119179cb5bb0bb4
Shilan Patel <shilan.patel@gmail.com>
2026-06-04 15:29:25 +0100

    Fixed stupid gitignore issue

---
+++ TubeService/App/Dependencies/AppDependencies+Stub.swift
@@ -0,0 +1,33 @@
+import CoreLocation
+import DataStores
+import Foundation
+
+#if DEBUG
+
+    extension AppDependencies {
+        static let stub: AppDependencies = {
+            print("WTF")
+            let locationManager = StubLocationManager()
+            locationManager.locationsToReturnOnRequest = [
+                CLLocation(latitude: 51.530823, longitude: -0.123821)
+            ]
+
+            return AppDependencies(
+                transportAPI: StubTransportAPIClient(),
+                systemStatusAPI: StubSystemStatusAPIClient(),
+                locationManager: locationManager,
+                localSearchCompleterClient: StubLocalSearchCompleterClient(),
+                userDefaults: .stub
+            )
+        }()
+    }
+
+    extension AppDataStore {
+        static let stub: AppDataStore = .init(dependencies: .stub)
+    }
+
+    extension UserDefaultsProvider {
+        static let stub = UserDefaultsProvider(.init(suiteName: "StubbedSuite")!)
+    }
+
+#endif
