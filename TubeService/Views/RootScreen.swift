218ea2488a662269cebe653bf9a309d7e0dee08d
Shilan Patel <shilan.patel@gmail.com>
2026-06-08 15:18:43 +0100

    Extract tabs to private vars

--- TubeService/Views/RootScreen.swift
+++ TubeService/Views/RootScreen.swift
@@ -6,46 +6,62 @@ struct RootScreen: View {
 
     var body: some View {
         TabView {
-            Tab {
-                LineStatusScreen()
-            } label: {
-                Label(.lineStatusTabTitle, systemImage: "info.circle")
-            }
-            .accessibilityIdentifier("acc.id.line.status.tab.title")
-
-            Tab {
-                JourneyPlannerScreen()
-            } label: {
-                Label(.journeyPlannerTabTitle, systemImage: "arrow.triangle.swap")
-            }
-            .accessibilityIdentifier("acc.id.journey.planner.tab.title")
-
-            Tab {
-                ArrivalsPickerScreen()
-            } label: {
-                Label(.arrivalsTabTitle, systemImage: "tram")
-            }
-            .accessibilityIdentifier("acc.id.arrivals.tab.title")
-
-            Tab {
-                NearbyStationsScreen()
-            } label: {
-                Label(.nearbyStationsTabTitle, systemImage: "location.magnifyingglass")
-            }
-            .accessibilityIdentifier("acc.id.nearby.stations.tab.title")
-
-            Tab {
-                MapsScreen()
-            } label: {
-                Label(.mapsTabTitle, systemImage: "map")
-            }
-            .accessibilityIdentifier("acc.id.maps.tab.title")
+            lineStatusTab
+            journeyPlannerTab
+            liveArrivalsTab
+            nearbyStationsTab
+            mapsTab
         }
         .tabViewStyle(.sidebarAdaptable)
         .systemStatusRefreshable()
         .task {
             appData.initialiseAppData()
         }
+    }
+    
+    private var lineStatusTab: some TabContent<Never> {
+        Tab {
+            LineStatusScreen()
+        } label: {
+            Label(.lineStatusTabTitle, systemImage: "info.circle")
+        }
+        .accessibilityIdentifier("acc.id.line.status.tab.title")
+    }
+    
+    private var journeyPlannerTab: some TabContent<Never> {
+        Tab {
+            JourneyPlannerScreen()
+        } label: {
+            Label(.journeyPlannerTabTitle, systemImage: "arrow.triangle.swap")
+        }
+        .accessibilityIdentifier("acc.id.journey.planner.tab.title")
+    }
+    
+    private var liveArrivalsTab: some TabContent<Never> {
+        Tab {
+            ArrivalsPickerScreen()
+        } label: {
+            Label(.arrivalsTabTitle, systemImage: "tram")
+        }
+        .accessibilityIdentifier("acc.id.arrivals.tab.title")
+    }
+    
+    private var nearbyStationsTab: some TabContent<Never> {
+        Tab {
+            NearbyStationsScreen()
+        } label: {
+            Label(.nearbyStationsTabTitle, systemImage: "location.magnifyingglass")
+        }
+        .accessibilityIdentifier("acc.id.nearby.stations.tab.title")
+    }
+    
+    private var mapsTab: some TabContent<Never> {
+        Tab {
+            MapsScreen()
+        } label: {
+            Label(.mapsTabTitle, systemImage: "map")
+        }
+        .accessibilityIdentifier("acc.id.maps.tab.title")
     }
 }
 
